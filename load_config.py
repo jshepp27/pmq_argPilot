cb9cef4fa399375bbf751a48bedb5996f662ca19
jshepp27 <36790461+jshepp27@users.noreply.github.com>
2023-01-06 14:17:46 +0000

    early api experiments

--- load_config.py
+++ load_config.py
@@ -9,7 +9,13 @@ with open("config.json") as config:
     logger.info(">>> Loading Token ... ")
     data = json.load(config)
     token = data["KEY"]
+    api_key = data["TWITTER_KEY"]
+    api_secret = data["TWITTER_SECRET"]
+    bearer_token = data["BEARER_TOKEN"]
 
 # LOAD TOKEN
 os.environ["KEY"] = token
+os.environ["BEARER_TOKEN"] = bearer_token
+os.environ["TWITTER_KEY"] = api_key
+os.environ["TWITTER_SECRET"] = api_secret
 logger.info(">>> Token in Environment ... ")
